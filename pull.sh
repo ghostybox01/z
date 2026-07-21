1f3ea5eb70d25c805acee63c9ec06050886910e2
ghostybox01 <john.hill@coralcoastcollections.com>
2026-07-20 20:44:17 -0400

    fix: add missing safe_urlopen.py, urlopen_compat.py, ssh_helper.py to pull.sh

--- pull.sh
+++ pull.sh
@@ -4,7 +4,8 @@ REPO="https://raw.githubusercontent.com/ghostybox01/z/main"
 CORE=/opt/synthtel/core
 WEB=/var/www/html
 
-for f in server.py campaign.py api_sender.py b2b_manager.py crm_sender.py \
+for f in server.py safe_urlopen.py urlopen_compat.py ssh_helper.py \
+          campaign.py api_sender.py b2b_manager.py crm_sender.py \
           email_checker.py email_sorter.py imap_extractor.py link_encoder.py \
           mime_builder.py mx_sender.py o365_relay.py owa_sender.py smtp_sender.py \
           spam_filter.py suppression_list.py tags.py telegram_bot.py tunnel_manager.py; do
